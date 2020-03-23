f0fcf5c8c5d1d0bad75b3fe2444e3e461737b8de
youinho <akdlkg@naver.com>
2020-03-23 18:06:08 +0900

    0323 v1.4 part1,2,3

--- code/part2(Chart)/bar.py
+++ code/part2(Chart)/bar.py
@@ -16,11 +16,31 @@ font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get
 rc('font', family=font_name)
 
 y_value = (float(death_ratio_30[:-1]),float(death_ratio_40[:-1]),float(death_ratio_50[:-1]),float(death_ratio_60[:-1]),float(death_ratio_70[:-1]),float(death_ratio_80[:-1]))
-x_name=('30대','40대','50대','60대','70대','80대')
+x_name=['30대','40대','50대','60대','70대','80대']
 n_groups = len(x_name)
 index = np.arange(n_groups)
 
-plt.bar(index, y_value, tick_label=x_name, align='center')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.bar(index, y_value, tick_label=x_name, align='center')
+
+for p in ax.patches :
+    left, bottom, width, height = p.get_bbox().bounds
+    ax.annotate("{}%".format(height), (left+width/2, height+0.1), ha='center')
+
+
+def move_cursor(event) :
+    x_name=['30대','40대','50대','60대','70대','80대']
+    if event.inaxes != ax:
+        return
+    x=event.xdata
+    y=event.ydata
+    print(x)
+    plt.text(x,y,x_name[int((x+0.5)//1)])
+    plt.draw()
+
+plt.connect('button_press_event',move_cursor)
+
 plt.xlabel('나이')
 plt.ylabel('치사율')
 plt.title('나이대별 치사율')
--- code/part2(Chart)/pie.py
+++ code/part2(Chart)/pie.py
@@ -14,11 +14,11 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(),encoding="utf-8")
 font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 rc('font', family=font_name)
 
-labels = ['male', 'female']
+labels = ['남성', '여성']
 sizes = [float(gender_m[:-1]),float(gender_w[:-1])]
 colors = ['red','blue']
 explode = (0.01,0.01)
-plt.title("gender")
+plt.title("성별 대비")
 plt.pie(sizes, explode=explode, labels=labels, colors=colors,
         autopct='%1.1f%%', shadow=True, startangle=90)
 plt.axis('equal')
