f89c42306761cdeea1731c2f21e80ea40541a3da
youinho <akdlkg@naver.com>
2020-03-31 16:35:06 +0900

    0331 V2.12 UI(Main)

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ import datetime
 from UI_Main import Ui_MainWindow
 import sys, io
 from PyQt5.QtMultimedia import QSound
-
+from code.part2(Chart).bar import bar_chart
+import pie
+import world_page
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(),encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(),encoding='utf-8')
@@ -30,7 +32,11 @@ class Main(QMainWindow,Ui_MainWindow):
         self.stackedWidget.setCurrentIndex(0)
         self.stackedWidget_local_view.setCurrentIndex(0)
 
-
+        # world_bar=w_b.world_chart(self.page_world)
+        domestic_pie=pie.pie_chart(self.page_local_view_3)
+        domestic_bar=bar.bar_chart(self.page_local_view_3)
+        world_bar = world_page.page_world(self.page_world)
+        world_bar.setGeometry(QtCore.QRect(50,150,700,700))
 
     def initSignal(self) :
     #----- push버튼 3종세트
@@ -50,13 +56,10 @@ class Main(QMainWindow,Ui_MainWindow):
         self.pushButton_world.clicked.connect(self.changeMain_1)
         self.pushButton_world.clicked.connect(self.changeCategory_1)
 
+
         # 지도
         self.pushButton_map.clicked.connect(self.changeMain_1)
         self.pushButton_map.clicked.connect(self.changeCategory_2)
-
-        self.pb_home_part1_1.clicked.connect(self.home_part_2)
-        self.pb_home_part2.clicked.connect(self.home_part_1)
-
 
 
     # 홈/메인 전환
@@ -65,9 +68,6 @@ class Main(QMainWindow,Ui_MainWindow):
         self.pushButton_local.setEnabled(True)
         self.pushButton_world.setEnabled(True)
         self.pushButton_map.setEnabled(True)
-
-
-
 
     def changeMain_1(self): # 메인
         self.stackedWidget.setCurrentIndex(1)
@@ -96,13 +96,6 @@ class Main(QMainWindow,Ui_MainWindow):
         self.pushButton_map.setEnabled(False)
 
 
-        # 홈 > 개요/예방수칙
-    def home_part_1(self) :
-        self.home_view_part.setCurrentIndex(0)
-
-    def home_part_2(self) :
-        self.home_view_part.setCurrentIndex(1)
-
 
     # 국내 버튼 누르고 > 하단 뷰창
     def changeLocalView_0(self):
@@ -111,7 +104,6 @@ class Main(QMainWindow,Ui_MainWindow):
         self.stackedWidget_local_view.setCurrentIndex(1)
     def changeLocalView_2(self):
         self.stackedWidget_local_view.setCurrentIndex(2)
-
 
 
 
